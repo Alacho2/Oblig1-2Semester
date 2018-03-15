26c06708739436b4bc86c9403c3e7fc34e016287
Håvard Nordlie Mathisen <mail@alacho.no>
2018-03-15 13:17:43 +0100

    Client.class

---
+++ src/Client.java
@@ -0,0 +1,21 @@
+public class Client {
+
+    public Client(){
+        mainMethod();
+    }
+
+    private void mainMethod() {
+        MeterArchive m = new MeterArchive();
+
+        m.printAllInstruments(); //Print the instrument
+        m.deleteInstrument("k300"); //Delete instrument with regNumber k300
+        m.getInstrument("k100"); //Get the instrument with regNumber k100
+        m.addInstrument(new Thermometer("k560", "H300R1", true, 100.5, -30)); //Add a new instrument
+        m.setInstrumentNewPosition("k560", "H300R2"); //Set new position for Instrument
+        m.setInstrumentNotWorking("k560"); //Set the instrument not working
+        m.printAllInstruments();
+    }
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        MeterArchive m = new MeterArchive();
-
-        m.printAllInstruments(); //Print the instrument
-        m.deleteInstrument("k300"); //Delete instrument with regNumber k300
-        m.getInstrument("k100"); //Get the instrument with regNumber k100
-        m.addInstrument(new Thermometer("k560", "H300R1", true, 100.5, -30)); //Add a new instrument
-        m.setInstrumentNewPosition("k560", "H300R2"); //Set new position for Instrument
-        m.setInstrumentNotWorking("k560"); //Set the instrument not working
-        m.printAllInstruments();
+        Client c = new Client();
     }
 }
